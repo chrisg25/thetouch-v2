681db851bfb308d6eb6329e83e1350dc19e485c0
unknown <arongrhyz@gmail.com>
2022-08-03 16:30:49 +0800

    added featured card component

---
+++ components/Home/Featured.tsx
@@ -0,0 +1,42 @@
+import { NextPage } from "next";
+
+interface FeaturedProps {
+  backgroundImage: string;
+  headline: string;
+  writer: string;
+  dateTime: string;
+}
+
+const Featured: NextPage<FeaturedProps> = ({
+  backgroundImage,
+  headline,
+  writer,
+  dateTime,
+}) => {
+  return (
+    <div className="home__featured">
+      <div
+        className="home__featured-photo-container"
+        style={{
+          backgroundImage,
+        }}
+      ></div>
+      <div className="home__featured-details">
+        <h1 className="home__featured-headline">{headline}</h1>
+        <hr />
+        <h3 className="home__featured-writer">by {writer}</h3>
+        <div className="home__featured-date-button-container">
+          <p className="home__featured-date">{dateTime}</p>
+          <button className="home__featured-read-button">READ</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
+
+//
+//
+//
+//
