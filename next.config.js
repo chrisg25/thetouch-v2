bb861c4f4c6617668dc74accd06c2587405c710e
unknown <arongrhyz@gmail.com>
2022-08-03 13:08:14 +0800

    added external domains for external assets

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["thetouchpublication.site", "res.cloudinary.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
