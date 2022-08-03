7651ed3b143baf6baac7e154b9be3dcadc727d70
unknown <arongrhyz@gmail.com>
2022-08-04 06:10:19 +0800

    added articles data array

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,27 @@ interface HomePageProps {
 }
 
 const Home: NextPage<HomePageProps> = ({ pathname }) => {
+  const data = [
+    {
+      backgroundImage:
+        "https://res.cloudinary.com/rhyzschoolwebapp/image/upload/v1636900975/thetouchpub_article_banners/rwtfnhih1ftxaaplaaur.jpg",
+      writer: "Lester Janito",
+      headline: "NEWS | Pub renewal: Ex adviser Malongo speaks",
+    },
+    {
+      backgroundImage:
+        "https://res.cloudinary.com/rhyzschoolwebapp/image/upload/v1636901134/thetouchpub_article_banners/wqj4rzvy1tl8umjynm5b.jpg",
+      writer: "Lester Janito",
+      headline: "Feature | Elijah and his Series of Unfortunate Events",
+    },
+    {
+      backgroundImage:
+        "https://res.cloudinary.com/rhyzschoolwebapp/image/upload/v1636901157/thetouchpub_article_banners/nvjsswa2prwgv2kkqfr4.jpg",
+      writer: "Lester Janito",
+      headline:
+        "NEWS | TAMBAYAYONG &apos;21: First Online Leadership Training of SGCD - II",
+    },
+  ];
   return (
     <section className="home">
       <Featured
@@ -20,7 +41,7 @@ const Home: NextPage<HomePageProps> = ({ pathname }) => {
       <h3 className="home__recently-added-articles-description">
         Recently added articles
       </h3>
-      <FeaturedArticlesContainer />
+      <FeaturedArticlesContainer articles={data} />
       <p className="home__more-articles-button">
         <Link href={"/articles"}>More Articles</Link>
         <span className="home__more-articles-button-icon">
