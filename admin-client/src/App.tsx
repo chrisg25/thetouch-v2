5f5ee7a7ef081ba0554fa5be85dd4956bb3032bd
unknown <arongrhyz@gmail.com>
2022-08-22 21:04:48 +0800

    removed imports from deleted components

--- admin-client/src/App.tsx
+++ admin-client/src/App.tsx
@@ -1,7 +1,6 @@
 import { FC, Fragment } from "react";
 import Header from "./components/layout/Header";
 import Sidebar from "./components/layout/Sidebar";
-import AddArticles from "./pages/AddArticles";
 import AddJournalist from "./pages/AddJournalist";
 import { Routes, Route } from "react-router-dom";
 import AddArticle from "./pages/AddArticle";
@@ -13,7 +12,6 @@ const App: FC = () => {
       <Header />
       <Routes>
         <Route path="/" element={<AddArticle />} />
-        <Route path="/add-articles" element={<AddArticles />} />
         <Route path="/add-journalists" element={<AddJournalist />} />
       </Routes>
     </Fragment>
