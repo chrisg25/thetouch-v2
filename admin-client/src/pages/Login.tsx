5cad9be34122bb14c87ee7fdece18370b952334b
unknown <arongrhyz@gmail.com>
2022-08-27 13:31:31 +0800

    added styling for login page

--- admin-client/src/pages/Login.tsx
+++ admin-client/src/pages/Login.tsx
@@ -1,13 +1,53 @@
 import React from "react";
-import CustomInput from "../components/inputs/CustomInput";
+import userLogo from "../assets/user-solid.svg";
+import passwordLogo from "../assets/key-solid.svg";
+import Header from "../components/layout/Header";
+import touchIcon from "../assets/touch-icon.png";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Good day Admin! Please Login to access the dashboard</h1>
-      <input type={"text"} placeholder="User Name" />
-      <input type={"password"} placeholder="Password" />
-    </div>
+    <>
+      <header
+        className="header"
+        style={{ display: "flex", alignItems: "center" }}
+      >
+        <img src={touchIcon} alt="touch-icon" style={{ marginRight: "20px" }} />
+        <h1 className="header__title">
+          The Touch Admin | <span className="header__sub-title">Login</span>
+        </h1>
+      </header>
+      <div className="login">
+        <section>
+          <h1 className="login__title">
+            Good day Admin! Please Login to access the dashboard
+          </h1>
+          <div className="login__input-container">
+            <div className="login__input-icon-container">
+              <img src={userLogo} alt="" />
+            </div>
+            <input
+              className="login__input"
+              type={"text"}
+              placeholder="Username"
+            />
+          </div>
+          <div className="login__input-container">
+            <div className="login__input-icon-container">
+              <img src={passwordLogo} alt="" />
+            </div>
+            <input
+              className="login__input"
+              type={"password"}
+              placeholder="Password"
+            />
+          </div>
+          <button className="login__button">LOGIN</button>
+        </section>
+      </div>
+      <footer className="login__footer">
+        <h1>All Rights Reserved</h1>
+      </footer>
+    </>
   );
 };
 
