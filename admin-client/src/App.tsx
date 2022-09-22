5b071532e508de81b29f0b2f24a0cda0855641c1
unknown <arongrhyz@gmail.com>
2022-09-23 03:44:11 +0800

    removed nested routes (will fix later after investigation)

--- admin-client/src/App.tsx
+++ admin-client/src/App.tsx
@@ -2,8 +2,9 @@ import { FC, Fragment, useContext } from "react";
 import AddJournalist from "./pages/AddJournalist";
 import { Routes, Route } from "react-router-dom";
 import AddArticle from "./pages/AddArticle";
-import Layout from "./components/layout";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
+
 import AuthContext, { AuthContextProvider } from "./store/auth-context";
 
 const App: FC = () => {
@@ -12,10 +13,9 @@ const App: FC = () => {
     <AuthContextProvider>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Layout />}>
-          <Route path="/add-articles" element={<AddArticle />} />
-          <Route path="/add-journalists" element={<AddJournalist />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-articles" element={<AddArticle />} />
+        <Route path="/add-journalists" element={<AddJournalist />} />
       </Routes>
     </AuthContextProvider>
   );
