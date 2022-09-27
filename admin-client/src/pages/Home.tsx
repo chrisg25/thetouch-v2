4969815f8671cf83f59086d43e9d25a310f0deff
unknown <arongrhyz@gmail.com>
2022-09-27 10:33:31 +0800

    added dashboard home page layout with styles

--- admin-client/src/pages/Home.tsx
+++ admin-client/src/pages/Home.tsx
@@ -4,7 +4,74 @@ const Home = () => {
   return (
     <Layout>
       <div className="home">
-        <h1>Home</h1>
+        {/* Item */}
+        <div className="home__article-item">
+          <div style={{ width: "122px" }}>
+            <img
+              style={{ width: "100%", height: "142px", objectFit: "cover" }}
+              className="home__article-item-photo"
+              src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg"
+              alt=""
+            />
+          </div>
+
+          <div className="article-item-details">
+            <h1 className="home__article-title">
+              NEWS | Pub renewal: Ex-Adviser Malongo speaks
+            </h1>
+            <p className="home__article-author">by Lester Janito</p>
+            <div className="home__buttons-container">
+              <button>Edit</button>
+              <button>Delete</button>
+            </div>
+          </div>
+        </div>
+
+        {/* Item */}
+        <div className="home__article-item">
+          <div style={{ width: "122px" }}>
+            <img
+              style={{ width: "100%", height: "142px", objectFit: "cover" }}
+              className="home__article-item-photo"
+              src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg"
+              alt=""
+            />
+          </div>
+
+          <div className="article-item-details">
+            <h1 className="home__article-title">
+              NEWS | Pub renewal: Ex-Adviser Malongo speaks
+            </h1>
+            <p className="home__article-author">by Lester Janito</p>
+            <div className="home__buttons-container">
+              <button>Edit</button>
+              <button>Delete</button>
+            </div>
+          </div>
+        </div>
+
+        {/* Item */}
+        <div className="home__article-item">
+          <div style={{ width: "122px" }}>
+            <img
+              style={{ width: "100%", height: "142px", objectFit: "cover" }}
+              className="home__article-item-photo"
+              src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg"
+              alt=""
+            />
+          </div>
+
+          <div className="article-item-details">
+            <h1 className="home__article-title">
+              NEWS | Pub renewal: Ex-Adviser Malongo speaks
+            </h1>
+            <p className="home__article-author">by Lester Janito</p>
+            <div className="home__buttons-container">
+              <button>Edit</button>
+              <button>Delete</button>
+            </div>
+          </div>
+        </div>
       </div>
     </Layout>
   );
