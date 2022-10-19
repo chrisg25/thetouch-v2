c3ad74510c6123b95db921d9a0859aa418e98422
unknown <arongrhyz@gmail.com>
2022-10-19 23:01:46 +0800

    implemented dynamic page descrition when changing page location

--- admin-client/src/components/layout/Header.tsx
+++ admin-client/src/components/layout/Header.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  const { pathname } = useLocation();
+
+  const [currPathName, setCurrPathName] = useState<string>("Home");
+
+  useEffect(() => {
+    setCurrPathName(() => {
+      return pathname !== "/"
+        ? pathname
+            .replace("/", "")
+            .split("-")
+            .map((chr) => chr.charAt(0).toUpperCase() + chr.slice(1))
+            .join(" ")
+        : "Home";
+    });
+  }, []);
+
   return (
     <header className="header">
       <h1 className="header__title">
-        The Touch Admin | <span className="header__sub-title">Add Article</span>
+        The Touch Admin |{" "}
+        <span className="header__sub-title">{currPathName}</span>
       </h1>
     </header>
   );
