8ca2a07ac565c14f362c26acbdec37f5676185e1
unknown <arongrhyz@gmail.com>
2022-11-17 14:42:32 +0800

    implemented fetch journalists on page load

--- admin-client/src/pages/Journalists.tsx
+++ admin-client/src/pages/Journalists.tsx
@@ -1,16 +1,39 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Layout from "../components/layout";
 import AuthContext from "../store/auth-context";
+import { JournalistType } from "../types";
 
 const Journalists = () => {
   const authContext = useContext(AuthContext);
+  const [journalists, setJournalists] = useState<JournalistType[]>([]);
+  const [fetchError, setFetchError] = useState<boolean>(false);
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [isDeleting, setIsDeleting] = useState<boolean>(false);
+  const [toBeDeletedJournalist, setToBeDeletedJournalist] = useState<
+    number | null
+  >(null);
   const navigate = useNavigate();
+
   useEffect(() => {
     if (!authContext?.isLoggedIn) {
       navigate("/login");
     }
   }, [authContext?.isLoggedIn]);
+
+  useEffect(() => {
+    const fetchdArticles = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/articles/pagination/0");
+        const data: JournalistType[] = await res.json();
+        setJournalists(() => [...data]);
+      } catch (error) {
+        setFetchError((prevErr) => !prevErr);
+      }
+    };
+    fetchdArticles();
+  }, []);
+
   return (
     <Layout>
       <div className="journalists">
